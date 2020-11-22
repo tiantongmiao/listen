e7b5c9376681e02035d5b3c812ce49d9d4324f81
laimua <893154844@qq.com>
2020-11-23 00:37:53 +0800

    update pagehelper

--- module/pagehelper/PageHelper.js
+++ module/pagehelper/PageHelper.js
@@ -1,8 +1,8 @@
 class PageHelper {
   constructor(pageNum, pageSize, where) {
-    this.pageNum = pageNum
+    this.pageNum = pageNum ? pageNum : 1
+    this.pageSize = pageSize ? pageSize : 10
     this.pageIndex = ((pageNum - 1) < 0 ? 0 : (pageNum - 1)) * pageSize
-    this.pageSize = pageSize
     this.where = where
   }
 }
